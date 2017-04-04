93eef8347a9881beccb208528146cf05c4e87fba
Walid <wnouh@teclib.com>
2017-04-04 13:49:06 +0200

    Fix audit type display during new audit creation

--- inc/deploycheck.class.php
+++ inc/deploycheck.class.php
@@ -316,9 +316,13 @@ class PluginFusioninventoryDeployCheck {
       /*
        * Build actions types list
        */
-      $checks_types = [];
-      foreach (self::getTypes() as $label => $data) {
-         $checks_types+= $data;
+      if ($mode === 'create') {
+         $checks_types = self::getTypes();
+      } else {
+         $checks_types = [];
+         foreach (self::getTypes() as $label => $data) {
+            $checks_types+= $data;
+         }
       }
       array_unshift($checks_types, "---");
 
