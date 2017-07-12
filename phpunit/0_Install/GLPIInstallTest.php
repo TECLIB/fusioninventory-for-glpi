2c465a592c55af9bcf41567b37627484b51961f5
Walid <wnouh@teclib.com>
2017-07-12 18:39:02 +0200

    Update locales from master

--- phpunit/0_Install/GLPIInstallTest.php
+++ phpunit/0_Install/GLPIInstallTest.php
@@ -31,7 +31,7 @@ class GLPIInstallTest extends PHPUnit_Framework_TestCase {
       } else if ($glpi_version == '0.85/bugfixes') {
          $glpisql = '0.85.5';
       } else {
-         $glpisql = '9.1.4';
+         $glpisql = '9.1.3';
       }
 
       $result = load_mysql_file(
