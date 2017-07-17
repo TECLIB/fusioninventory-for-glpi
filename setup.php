1bec48fb9306c6930f2782380ae2d3b6a26756f8
Walid <wnouh@teclib.com>
2017-07-17 18:24:56 +0200

    Update version

--- setup.php
+++ setup.php
@@ -40,7 +40,7 @@
    ------------------------------------------------------------------------
  */
 
-define ("PLUGIN_FUSIONINVENTORY_VERSION", "0.90+1.3+TECLIB_21");
+define ("PLUGIN_FUSIONINVENTORY_VERSION", "0.90+1.3+TECLIB_22");
 
 // Used for use config values in 'cache'
 $PF_CONFIG = array();
