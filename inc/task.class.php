58a4cfd5468e97e6496e0cc3ca242520849ff2ac
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2017-11-15 15:08:45 +0100

    agents_postponed doesn't exist

--- inc/task.class.php
+++ inc/task.class.php
@@ -1299,7 +1299,6 @@ class PluginFusioninventoryTask extends PluginFusioninventoryTaskView {
 
                      $agent_state = 'success';
                      break;
-
                }
                if (!isset($counters['agents_error'][$agent_id])
                        and !isset($counters['agents_success'][$agent_id])) {
